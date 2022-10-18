ff83ce6b0afff9d5a7ae3b81b9b9176dfbee93f1
bansheerubber <gary.cactus.email@gmail.com>
2022-10-18 13:09:51 -0700

    add basic grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct DSLParser;
+
 fn main() {
-    println!("Hello, world!");
+	let result = DSLParser::parse(Rule::program, "printf(#5);");
+	println!("{:?}", result);
 }
