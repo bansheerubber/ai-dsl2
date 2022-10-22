2046b696f61bd0d6d0333e208b8e2e1a3703331b
bansheerubber <gary.cactus.email@gmail.com>
2022-10-21 19:23:53 -0700

    code_generator: reflect compiler bit/pointer awareness for types

--- code_generator/src/types.rs
+++ code_generator/src/types.rs
@@ -2,9 +2,9 @@ use ai_dsl2_compiler::Type;
 
 pub fn convert_type_name(type_name: &str) -> Type {
 	match type_name {
-		"float" => Type::Float,
-		"int" => Type::Integer,
-		"string" => Type::CString,
+		"float" => Type::Float(0),
+		"int" => Type::Integer(0, 64),
+		"string" => Type::CString(0),
 		&_ => todo!(),
 	}
 }
