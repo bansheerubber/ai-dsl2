a4d18b5e93a523da03e3346f11fbe9ee9f4410f7
bansheerubber <gary.cactus.email@gmail.com>
2023-02-03 17:23:01 -0700

    pass function name into runtime

--- code_generator/src/compiler/learned_value.rs
+++ code_generator/src/compiler/learned_value.rs
@@ -6,8 +6,11 @@ pub struct LearnedValue;
 
 impl LearnedValue {
 	pub fn compile(context: &mut CompilationContext) -> Value {
+		let function_name = context.module.function_table.get_function(&context.current_function.as_ref().unwrap())
+			.unwrap().name.clone();
+
 		let mut args = vec![
-			context.module.create_global_string(context.current_block.unwrap(), "hey there"),
+			context.module.create_global_string(context.current_block.unwrap(), &function_name),
 			context.module.create_immediate_integer(0),
 			context.module.create_immediate_integer(0),
 		];
