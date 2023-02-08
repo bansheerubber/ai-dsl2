8e6e5b7df7042b2d69eee551ea68aaf37e1b66b9
bansheerubber <gary.cactus.email@gmail.com>
2023-02-08 13:30:16 -0700

    code_generator: store learned values in `Function` compiler struct

--- code_generator/src/compiler/learned_value.rs
+++ code_generator/src/compiler/learned_value.rs
@@ -14,8 +14,14 @@ impl LearnedValue {
 			context.module.create_immediate_integer(0),
 			context.module.create_immediate_integer(0),
 		];
-		context.module.add_function_call(
+
+		let learned_value = context.module.add_function_call(
 			context.current_block.unwrap(), &context.placeholder_evaluation_int, &mut args
-		)
+		);
+
+		let function = context.module.function_table.get_function_mut(&context.current_function.as_ref().unwrap()).unwrap();
+		function.add_learned_value(learned_value);
+
+		return learned_value;
 	}
 }
