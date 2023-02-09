a5838b7f19726c6a9f1508da59874c43483aca8b
bansheerubber <gary.cactus.email@gmail.com>
2023-02-08 21:08:47 -0700

    code_generator: add function args to the `VariableTable`

--- code_generator/src/compiler/function.rs
+++ code_generator/src/compiler/function.rs
@@ -11,7 +11,8 @@ impl Function {
 		let mut name = "";
 		let mut return_type = "";
 
-		let mut argument_types = vec![];
+		let mut argument_names = Vec::new();
+		let mut argument_types = Vec::new();
 		let pairs = pair.into_inner();
 		for pair in pairs.clone() {
 			if pair.as_rule() == parser::Rule::token {
@@ -24,16 +25,30 @@ impl Function {
 				for argument_pair in pair.into_inner() {
 					if argument_pair.as_rule() == parser::Rule::type_token {
 						argument_types.push(convert_type_name(argument_pair.as_str()));
+					} else if argument_pair.as_rule() == parser::Rule::token {
+						argument_names.push(String::from(argument_pair.as_str()));
 					}
 				}
 			}
 		}
 
-		context.current_function = Some(
-			context.module.create_function(name, &argument_types, convert_type_name(return_type))
-		);
+		context.current_function = Some(context.module.create_function(
+			name,
+			&argument_types,
+			convert_type_name(return_type)
+		));
+
 		let block = context.module.new_block(name, &context.current_function.as_ref().unwrap());
 		context.current_block = Some(block);
+
+		for i in 0..argument_names.len() {
+			let argument_name = &argument_names[i];
+			let argument_type = argument_types[i];
+			let function = context.module.function_table.get_function(&context.current_function.as_ref().unwrap()).unwrap();
+			let argument_value = function.get_argument(i);
+
+			context.module.add_argument(block, &argument_name, argument_type, argument_value);
+		}
 
 		compile_pairs(context, pairs.last().unwrap().into_inner());
 
