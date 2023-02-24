abab63f977081895605cdeebf9d8adb760f28272
bansheerubber <gary.cactus.email@gmail.com>
2023-02-23 17:51:53 -0700

    code_generator: fix compilation of function calls w/o arguments

--- code_generator/src/compiler/function_call.rs
+++ code_generator/src/compiler/function_call.rs
@@ -14,19 +14,23 @@ impl FunctionCall {
 
 		let function_name = context.module.transform_function_name(pairs.next().unwrap().as_str());
 
-		// compile the arguments
-		let mut args = Vec::new();
-		for pair in pairs.clone().next().unwrap().into_inner() {
-			let Some(value) = compile_pair(context, pair) else {
-				unreachable!();
-			};
+		let mut argument_values = Vec::new();
 
-			args.push(value);
+		let arguments = pairs.next();
+		if let Some(arguments) = arguments {
+			// compile the arguments
+			for pair in arguments.into_inner() {
+				let Some(value) = compile_pair(context, pair) else {
+					unreachable!();
+				};
+
+				argument_values.push(value);
+			}
 		}
 
 		let key = FunctionKey::new(&function_name);
 		context.module.add_function_call(
-			context.current_block.unwrap(), &key, &mut args
+			context.current_block.unwrap(), &key, &mut argument_values
 		)
 	}
 }
