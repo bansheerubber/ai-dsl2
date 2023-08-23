229415f7ed0af0c74adf21f5a367834cc972f13f
bansheerubber <gary.cactus.email@gmail.com>
2023-08-22 23:10:03 -0700

    code_generator: implement assigning to object properties

--- code_generator/src/compiler/variable_assignment.rs
+++ code_generator/src/compiler/variable_assignment.rs
@@ -9,17 +9,43 @@ pub struct VariableAssignment;
 impl VariableAssignment {
 	pub fn compile(context: &mut CompilationContext, pair: Pair<parser::Rule>) -> Value {
 		let property_assignment = pair.as_rule() == parser::Rule::property_assignment;
-
 		let mut pairs = pair.into_inner();
 
-		let variable_name = pairs.next().unwrap().as_str();
-		let variable = context.module.get_variable(context.current_block.unwrap(), variable_name).unwrap();
+		if property_assignment {
+			let mut property_chain = pairs.next().unwrap().into_inner();
+			let mut value = context.module.get_variable(
+				context.current_block.unwrap(),
+				property_chain.next().unwrap().as_str()
+			).unwrap();
+			let assignment_value = compile_pair(context, pairs.last().unwrap()).unwrap();
 
-		let value = compile_pair(context, pairs.last().unwrap()).unwrap();
-		context.module.add_store(
-			context.current_block.unwrap(),
-			variable,
+			let property_chain = property_chain.collect::<Vec<Pair<parser::Rule>>>();
+			let length = property_chain.len();
+			for i in 0..length {
+				let property_name = property_chain.get(i).unwrap().as_str();
+				if i == length - 1 {
+					value = context.module.add_store_to_obj(
+						context.current_block.unwrap(),
+						value, // use last value in the chain, should always be an object
+						property_name,
+						assignment_value,
+					).unwrap();
+				} else {
+					// TODO something, you know what
+				}
+			}
+
 			value
-		).unwrap()
+		} else {
+			let variable_name = pairs.next().unwrap().as_str();
+			let variable = context.module.get_variable(context.current_block.unwrap(), variable_name).unwrap();
+
+			let value = compile_pair(context, pairs.last().unwrap()).unwrap();
+			context.module.add_store(
+				context.current_block.unwrap(),
+				variable,
+				value
+			).unwrap()
+		}
 	}
 }
